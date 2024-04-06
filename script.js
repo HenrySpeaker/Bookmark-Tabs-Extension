920738b097d4ad89cb83f599f0305c303685733d
Henry Speaker <73503466+HenrySpeaker@users.noreply.github.com>
2024-04-06 03:24:30 -0400

    Begin creating form functionality

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 const windows = await chrome.windows.getAll();
 const tabs = await Promise.all(windows.map((window) => chrome.tabs.query({ windowId: window.id })));
+
+const addBtn = document.getElementById("add-bookmarks-btn");
+
+function addBookmarks() {
+  console.log("add bookmarks");
+}
+
+document.getElementById("add-bookmarks-form").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (e.target === addBtn) {
+    addBookmarks();
+  }
+});
